4a05eb18e5c44e49fcab15d0bb76a2d603f54397
szymanskir <ryszard.szymanski@outlook.com>
2018-04-04 14:44:59 +0200

    Add mfcc derivatives

--- DTW.py
+++ DTW.py
@@ -1,5 +1,4 @@
-import numpy as np
-from numpy import linalg
+from numpy import linalg, zeros
 
 
 def dynamic_time_warping(x, y):
@@ -11,13 +10,13 @@ def dynamic_time_warping(x, y):
     """
 
     # create local distance matrix
-    distance_matrix = np.zeros((len(x), len(y)), dtype=np.int16);
+    distance_matrix = zeros((len(x), len(y)));
     for i in range(0, len(x)):
         for j in range(0, len(y)):
             distance_matrix[i, j] = linalg.norm(x[i] - y[j])
 
     # initialize dtw matrix
-    dtw_matrix = np.zeros((len(x), len(y)), dtype=np.int16)
+    dtw_matrix = zeros((len(x), len(y)))
     dtw_matrix[0, 0] = distance_matrix[0, 0]
 
     # first column initialization
@@ -31,47 +30,7 @@ def dynamic_time_warping(x, y):
     # calculate remaining dtw_matrix elements
     for i in range(1, len(x)):
         for j in range(1, len(y)):
-            dtw_matrix[i, j] = min(dtw_matrix[i-1, j-1], dtw_matrix[i-1, j], dtw_matrix[i, j-1])
-            + distance_matrix[i, j]
+            dtw_matrix[i, j] = min(dtw_matrix[i-1, j-1], dtw_matrix[i-1, j], dtw_matrix[i, j-1]) + distance_matrix[i, j]
 
-    """
-    pairs = [(len(x) - 1, len(y) - 1)]
-    i = len(x) - 1
-    j = len(y) - 1
-
-    while i != 0 or j != 0:
-        if i == 0:
-            --j
-            continue
-        elif j == 0:
-            --i
-            continue
-
-        smallest_cost = dtw_matrix[i-1, j-1]
-        step_i = -1
-        step_j = -1
-        if smallest_cost > dtw_matrix[i-1, j]:
-            smallest_cost = dtw_matrix[i-1, j]
-            step_j = 0
-
-        if smallest_cost > dtw_matrix[i, j-1]:
-            step_i = 0
-            step_j = -1
-
-        i += step_i
-        j += step_j
-        last = pairs[0]
-
-        if last[0] != i and last[1] != j:
-            pairs.append((i, j))
-
-    plt.plot(range(0, len(x)), x)
-    plt.plot(range(0, len(y)), y)
-
-    for p in pairs:
-        plt.plot(p, (x[p[0]], y[p[1]]), 'r')
-
-    plt.show()
-    """
     return dtw_matrix[len(x)-1, len(y)-1]
 
--- MFCC.py
+++ MFCC.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.io.wavfile
 import scipy.fftpack
+import matplotlib.pyplot as plt
 
 
 def pre_emphasis(x, a=0.97):
@@ -10,7 +11,7 @@ def pre_emphasis(x, a=0.97):
     :param a: pre-emphasis coefficient
     :return: signal with enhanced higher frequencies
     """
-    x[1:] = x[1:] - a * x[0:len(x)-2]
+    x = x[1:] - a * x[:-1]
     return x
 
 
@@ -24,10 +25,14 @@ def voice_feature_extraction(infile):
     # read audio file
     sampling_rate, signal = scipy.io.wavfile.read(infile)
 
+    # signal pre_emphasis
+    signal = pre_emphasis(signal)
+
     # divide into n[ms] overlapping frames
-    n = 30
+    n = 25
+    step_ms = 10
     samples_per_frame = int(sampling_rate/1000 * n)
-    frame_step = int(samples_per_frame/2)
+    frame_step = int(sampling_rate/1000 * step_ms)
     frame_count = int(len(signal) / frame_step)
 
     frames = list()
@@ -37,7 +42,12 @@ def voice_feature_extraction(infile):
     # calculate MFCC vector for each frame
     feature_vector = list()
     for frame in frames:
-        feature_vector.append(MFCC(frame * np.hamming(len(frame)), sampling_rate))
+        mfcc = MFCC(frame * np.hamming(len(frame)), sampling_rate)
+        delta = derivative(mfcc)
+        delta_delta = derivative(delta)
+        mfcc = np.append(mfcc, delta)
+        mfcc = np.append(mfcc, delta_delta)
+        feature_vector.append(mfcc)
 
     return feature_vector
 
@@ -51,12 +61,66 @@ def MFCC(windowed_frame, sampling_rate):
 
     # calculate power spectrum
     spectrum = np.fft.fft(windowed_frame)
-    power_spectrum = np.square(np.abs(spectrum))
+    power_spectrum = np.square(np.abs(spectrum))/len(spectrum)
 
     # generate triangular filters
-    triangle_filters = np.ones((12, len(power_spectrum)))
+    triangle_filters = get_filter_bank(sampling_rate, len(spectrum))
 
     # Apply filters to power spectrum
-    S = triangle_filters * np.tile(power_spectrum, (12, 1))
+    S = triangle_filters.dot(power_spectrum)
+    S = np.where(S == 0, np.finfo(float).eps, S)
 
-    return scipy.fftpack.dct(np.log10(S), norm='ortho')[:, 1:13]
+    return lift(scipy.fftpack.dct(np.log(S), type=3, norm='ortho'))[1:12]
+
+
+def get_filter_bank(sampling_rate, signal_length, count=24, lower_bound=0, upper_bound=800):
+    lower_mel = frequency_to_mel(lower_bound)
+    upper_mel = frequency_to_mel(sampling_rate/2)
+
+    mel_samples = np.linspace(lower_mel, upper_mel, count + 2)
+    frequency_samples = mel_to_frequency(mel_samples)
+
+    frequency_bins = np.floor((signal_length+1) * frequency_samples / sampling_rate)
+
+    filter_bank = np.zeros([count, signal_length])
+    for j in range(0, count):
+        for i in range(int(frequency_bins[j]), int(frequency_bins[j + 1])):
+            filter_bank[j, i] = (i - frequency_bins[j]) / (frequency_bins[j + 1] - frequency_bins[j])
+        for i in range(int(frequency_bins[j + 1]), int(frequency_bins[j + 2])):
+            filter_bank[j, i] = (frequency_bins[j + 2] - i) / (frequency_bins[j + 2] - frequency_bins[j + 1])
+
+    #for j in range(0, count):
+    #   plt.plot(range(0,signal_length), filter_bank[j,:])
+
+    #plt.show()
+
+    return filter_bank
+
+
+def derivative(mfcc_vector, N=2):
+     return mfcc_vector[N:] - mfcc_vector[:-N]
+
+
+def lift(mfcc_vector, L=22):
+    n = np.arange(len(mfcc_vector))
+    return mfcc_vector * (1 + L/2 * np.sin(np.pi * n /L))
+
+
+
+def frequency_to_mel(f):
+    """
+    Converts frequency value(Hz) to mels
+    :param f: frequency value(Hz)
+    :return:
+    """
+    return 2595 * np.log10(1 + f/700.0)
+
+
+def mel_to_frequency(mel):
+    """
+    Converts mels to frequency value(Hz)
+    :param mel: mel value
+    :return:
+    """
+    return 700 * (10**(mel/2595.0) - 1)
+
