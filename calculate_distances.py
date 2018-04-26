1c089d07f22e549e11b24d7d560b0d86de41a754
szymanskir <ryszard.szymanski@outlook.com>
2018-04-26 16:59:54 +0200

    Add verification and cluster distances script

---
+++ calculate_distances.py
@@ -0,0 +1,42 @@
+import MFCC
+import DTW
+import numpy as np
+import sys
+import os
+import csv
+
+
+def calculate_within_cluster_distance(filepath):
+    feature_vectors = list()
+    samples = os.listdir(filepath)
+    for sample in samples:
+        feature_vectors.append(MFCC.voice_feature_extraction(os.path.join(filepath, sample)))
+
+    distances = list()
+    for i in range(0, len(feature_vectors)):
+        for j in range(i+1, len(feature_vectors)):
+            distances.append(DTW.dynamic_time_warping(feature_vectors[i], feature_vectors[j]))
+
+    return np.mean(distances)
+
+
+if len(sys.argv) != 2:
+    print("Usage: calculate_distance <path_to_database>")
+    exit(-1)
+
+database_filepath = sys.argv[1]
+persons = os.listdir(database_filepath)
+
+with open('cluster_distances.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['path', 'distance'])
+    for person in persons:
+        words = os.listdir(os.path.join(database_filepath, person))
+        for word in words:
+            samples_path = os.path.join(database_filepath, person, word)
+            print(samples_path)
+            distance = calculate_within_cluster_distance(samples_path)
+            writer.writerow([samples_path, distance])
+
+
+
---
+++ voice_verification.py
@@ -0,0 +1,27 @@
+import MFCC
+import DTW
+import os
+import numpy as np
+import pandas
+import sys
+
+
+def calculate_cluster_distance(sample, database):
+    distances = list()
+    sample_feature_vector = MFCC.voice_feature_extraction(sample)
+    for database_sample in os.listdir(database):
+        database_feature_vector = MFCC.voice_feature_extraction(os.path.join(database, database_sample))
+        distances.append(DTW.dynamic_time_warping(sample_feature_vector, database_feature_vector))
+
+    return np.mean(distances)
+
+
+def verify_voice_sample(sample, database):
+    distance = calculate_cluster_distance(sample, database)
+    df = pandas.read_csv('cluster_distances.csv')
+    series = df.loc[df['path'] == database]['distance']
+    return distance <= series.iloc[0]
+
+
+result = verify_voice_sample(sys.argv[1], sys.argv[2])
+print('Accepted' if result else 'Denied')
